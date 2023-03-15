64a25fc77b8267402f0abf27c8cab3d9a7f04ddc
Arjun Gopal <arjun.gopal84@gmail.com>
2023-03-15 18:14:02 -0500

    working AudioPlayer

---
+++ src/data/tracks.js
@@ -0,0 +1,52 @@
+// audio files
+import image from './image16.wav';
+import ttm from './ttm16.wav';
+import td from './td16.wav';
+import ntf from './ntf16.wav';
+import nml from './nml16.wav';
+import tn from './tn16.wav';
+
+// cover artrs
+import imageArt from './image-art.jpeg';
+import ttmArt from './ttm-art.jpeg';
+import timelessArt from './timeless-art.jpg';
+import epArt from './ep-art.jpeg';
+
+export const tracks = [
+    {
+        title: 'The Image',
+        src: image,
+        author: 'NaturoSynth',
+        thumbnail: imageArt,
+    },
+    {
+        title: 'Through the Motions',
+        src: ttm,
+        author: 'NaturoSynth',
+        thumbnail: ttmArt,
+    },
+    {
+        title: 'No More Love',
+        src: nml,
+        author: 'NaturoSynth',
+        thumbnail: epArt,
+    },
+    {
+        title: 'Telepathic Dream',
+        src: td,
+        author: 'NaturoSynth',
+        thumbnail: epArt,
+    },
+    {
+        title: 'Timeless Night',
+        src: tn,
+        author: 'NaturoSynth',
+        thumbnail: timelessArt
+    },
+    {
+        title: 'Never Too Far',
+        src: ntf,
+        author: 'NaturoSynth',
+        thumbnail: epArt,
+    },
+];
--- src/pages/epk.js
+++ src/pages/epk.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+import AudioPlayer from "../components/AudioPlayer/AudioPlayer";
 const EPK = () => {
     return (
         <div>
             <h1>
                 Promote our songs please
             </h1>
+            <AudioPlayer/>
         </div>
     );
 };
