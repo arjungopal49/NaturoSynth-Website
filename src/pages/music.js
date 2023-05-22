340470b53e2541e95b4739e7824fd412aabfb0b7
Arjun Gopal <arjun.gopal84@gmail.com>
2023-05-22 17:10:21 -0500

    added recent growth

--- src/pages/music.js
+++ src/pages/music.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SpotifyEmbeded from "../components/SpotifyEmbeded/SpotifyEmbeded";
+import AppleEmbeded from "../components/AppleEmbeded/AppleEmbeded";
 
 const Music = () => {
     return (
@@ -8,6 +9,7 @@ const Music = () => {
                 Listen to our new EP "In Control"
             </h1>
             <SpotifyEmbeded embedId={"/album/06aK2PcIMm933ZEbaWhr9P?utm_source=generator"}/>
+            <AppleEmbeded embedId={"/album/in-control-ep/1640268564"}/>
         </div>
     );
 };
