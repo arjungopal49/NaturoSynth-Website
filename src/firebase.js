5fc110cc1179836ba2cfd747cd6640b02f80b357
pjjosh <pranavjo@umich.edu>
2024-06-12 23:34:03 -0500

    firebase starter

---
+++ src/firebase.js
@@ -0,0 +1,65 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import {collection, getDocs, getFirestore, addDoc, deleteDoc, doc} from 'firebase/firestore/lite';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCi8hqYz2FqEzYfxfC9SoWtGEUqf-eqnqA",
+  authDomain: "naturosynth-backend.firebaseapp.com",
+  databaseURL: "https://naturosynth-backend-default-rtdb.firebaseio.com",
+  projectId: "naturosynth-backend",
+  storageBucket: "naturosynth-backend.appspot.com",
+  messagingSenderId: "105111682761",
+  appId: "1:105111682761:web:d537c1246c1f0e0efb5718",
+  measurementId: "G-JM7F50S1FH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function getCollection(collectionName) {
+  const collectionReference = collection(db, collectionName);
+  const snapshot = await getDocs(collectionReference);
+  return snapshot.docs.map(doc => doc.data());
+}
+
+export async function addToCollection(collectionName, data) {
+  // Add a new document with a generated id.
+  const myCollection = collection(db, collectionName);
+  await addDoc(myCollection, data);
+}
+
+export async function removeCollection(collectionName) {
+  let Ids = []
+  const querySnapshot = await getDocs(collection(db, collectionName));
+  querySnapshot.forEach((doc) => {
+      Ids.push(doc.id)
+  });
+  for (let i = 0; i < Ids.length; i++) {
+      await deleteDoc(doc(db, collectionName, Ids[i]));
+  }
+  let check = await getCollection(collectionName)
+  do{
+      await sleep(100)
+  }while (check.length > 0)
+}
+
+export async function getCollectionList(collectionName) {
+  const collectionReference = collection(db, collectionName);
+  const snapshot = await getDocs(collectionReference);
+  const list = [];
+  snapshot.forEach((doc) => {
+    // console.log(doc.id, " => ", doc.data());
+    list.push(doc.data());
+  });
+  return list;
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
--- src/pages/merch.js
+++ src/pages/merch.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { getCollection, getCollectionList } from "../firebase";
+import { useState, useEffect } from "react";
 
 const Merch = () => {
-    return (
+    const [songs, setSongs] = useState([]);
+
+    useEffect(() =>{ 
+        const getSongs = async() => {
+            const s = await getCollectionList("songs");
+            setSongs(s);
+        }
+        getSongs()
+    },[])
+
+
+    console.log(songs);
+// 192.168.1.105
+    return ( 
         <div className="merch-full">
             <h1>
                 New shirts coming soon!
             </h1>
+            {songs.map((song)=>
+                    <p> {song.title} </p>
+                )}
         </div>
     );
 };
